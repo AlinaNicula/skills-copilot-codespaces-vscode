d7731436a9ec26d218318f474e7efd0d86fea2dd
alina-nicula-visma <71430155+AlinaNicula@users.noreply.github.com>
2025-02-12 15:49:59 +0000

    Copilot 3rd commit

--- comments.js
+++ comments.js
@@ -25,6 +25,6 @@ app.post('/comments', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000');
+// });
