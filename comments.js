8e50a8de960f383a8b6e3da5eb78a43d7baca9af
alina-nicula-visma <71430155+AlinaNicula@users.noreply.github.com>
2025-02-12 15:44:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,30 @@
+//create a web server
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/comments', (req, res) => {
+    fs.readFile('./comments.json', 'utf8', (err, data) => {
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/comments', (req, res) => {
+    fs.readFile('./comments.json', 'utf8', (err, data) => {
+        const comments = JSON.parse(data);
+        comments.push(req.body);
+        fs.writeFile('./comments.json', JSON.stringify(comments, null, 4), (err) => {
+            res.json(req.body);
+        });
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
